b0e65f93613bb29426bc6faf844e5937517d02c4
Wesley <wesleycomp@gmail.com>
2022-05-30 15:06:23 -0300

    inicio projeto versao ainda do examplo

---
+++ api.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const queryString = require('querystring');
+
+async function publicCall(path, data, method = 'GET', headers = {}) {
+    try {
+        const qs = data ? `?${queryString.stringify(data)}` : '';
+        const result = await axios({
+            method,
+            url: `${process.env.API_URL}${path}${qs}`
+        });
+        return result.data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function time() {
+    return publicCall('/v3/time');
+}
+
+async function depth(symbol = 'BTCBRL', limit = 5) {
+    return publicCall('/v3/depth', { symbol, limit });
+}
+
+module.exports = { time, depth }
